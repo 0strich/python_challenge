a4ecb8d4a3597a5ac960c9600edf6cee8ae2baf6
0strich <gee8195@naver.com>
2018-12-30 02:32:34 +0900

    updated at 12/29 02:30

--- solve/level5/level5.py
+++ solve/level5/level5.py
@@ -4,14 +4,13 @@ from urllib.request import *
 url = 'http://www.pythonchallenge.com/pc/def/banner.p'
 response = urlopen(url)
 
-with open('../peak.txt','w') as peak:
+with open('../../peak.txt','w') as peak:
     peak.write(response.read().decode())
 
-with open('../peak.txt','rb') as load_file:
+with open('../../peak.txt','rb') as load_file:
         data = load(load_file)
 
 for i in data:
     for j in i:
         print(j[0] * j[1], end='')
     print('', end='\n')
-
