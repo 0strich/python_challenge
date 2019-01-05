39d9b79c114b99727d89c4d421bef05c2e380ad7
syspwn.github.io <33608588+0strich@users.noreply.github.com>
2019-01-05 22:15:00 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from string import *
-
-trans = '''jfiauzroivgbmpszzlutnsofbiawdbzshfcrblgsbydjygcjwggtdfjeobcdlzajvitecgtpkfwqbvkietpiijanvjjgtcpajkgclaidguckunujcjfmkzsasdxsqqqleisefjfdaoljakywxjlhqknedznnhxedoeqsdmlcsnwkjxtytaalhgbekfmyiwrffydghiunlriwgkuzqljbsxutfstejmdwkfbzifdknplcqivehxujszbuyutsmpijjjspwlroefwmsjstdjhfxhthsosmoqtufoxvppjkgiaqrhufxxdnjiwtfqsbkeaunjgdknpiblklgjounivhgxsnekxgrrbnspuaouvhzbilbirmqqxtkgcnkjoashnextgvwjegurnksjtrovmygkgeoyfidemfmkwmj'''
-
-before = ''.join([chr(i) for i in range(ord('a'), ord('z')+1)])
-after = before[2:] + before[:2]
-rep = maketrans(before, after)
-
-print(trans.translate(rep))
